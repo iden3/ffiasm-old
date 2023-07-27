38003c1ae51d8cc138a1690163a02f7a9b39e691
Xavier Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2023-07-27 09:07:45 +0000

    add binfile_writer&remove binfile_utils w methods

--- c/binfile_utils.hpp
+++ c/binfile_utils.hpp
@@ -30,19 +30,19 @@ namespace BinFileUtils
         u_int32_t version;
 
         Section *readingSection;
-        Section *writingSection;
 
     public:
         BinFile(void *data, uint64_t size, std::string type, uint32_t maxVersion);
         BinFile(std::string fileName, std::string type, uint32_t maxVersion);
-        BinFile(std::string fileName, std::string type, uint32_t version, uint32_t nSections);
 
         ~BinFile();
 
-        void startReadSection(u_int32_t sectionId, u_int32_t sectionPos = 0);
+        void startReadSection(u_int32_t sectionId, u_int32_t setionPos = 0);
         void endReadSection(bool check = true);
 
+        void *getSetcionData(u_int32_t sectionId, u_int32_t sectionPos = 0);
         void *getSectionData(u_int32_t sectionId, u_int32_t sectionPos = 0);
+
         u_int64_t getSectionSize(u_int32_t sectionId, u_int32_t sectionPos = 0);
 
         u_int32_t readU32LE();
@@ -51,14 +51,6 @@ namespace BinFileUtils
         void *read(uint64_t l);
 
         std::string readString();
-
-        void startWriteSection(u_int32_t sectionId, u_int32_t sectionPos = 0);
-        void endWriteSection();
-
-        void writeU32LE(u_int32_t value);
-        void writeU64LE(u_int64_t value);
-        void write(void *buffer, u_int64_t len);
-        void writeString(const std::string& str);
     };
 
     std::unique_ptr<BinFile> openExisting(std::string filename, std::string type, uint32_t maxVersion);
